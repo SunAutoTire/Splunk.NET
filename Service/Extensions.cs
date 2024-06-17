8b1675ef9a8e585aa50c7dfe07cdc9383445096d
Daniel Przybylski <daniel.przybylski@sun.auto>
2024-06-17 09:48:01 -0700

    Log errors

---
+++ Service/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+
+namespace SunAuto.Logging.Api;
+
+public static class Extensions
+{
+    internal static HttpResponseData HandleError(this ILogger logger, HttpRequestData requestData, Exception ex, HttpStatusCode statusCode)
+    {
+        logger.LogIt(statusCode, ex);
+
+        var response = requestData.CreateResponse(statusCode);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        var content = JsonSerializer.Serialize(new { ex.Message });
+        response.WriteString(content);
+
+        return response;
+    }
+
+    static void LogIt(this ILogger logger, HttpStatusCode statusCode, Exception ex)
+    {
+        switch (statusCode)
+        {
+            case HttpStatusCode.NotFound:
+                logger.LogWarning(ex, ex.Message);
+                break;
+            case HttpStatusCode.InternalServerError:
+            default:
+                logger.LogCritical(ex, ex.Message);
+                break;
+        }
+    }
+}
--- Service/LoggingApi.cs
+++ Service/LoggingApi.cs
@@ -1,30 +1,65 @@
-using System.Net;
-using System.Text.Json;
-using System.Threading;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
 
 namespace SunAuto.Logging.Api;
 
 public class LoggingApi
 {
-    [Function("HttpTrigger1")]
-    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "products/{category:alpha}/{id:int?}")] HttpRequestData req, string category, int? id,
-    FunctionContext executionContext)
+    [Function("LoggerItem")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", "delete", Route = "api/{application:alpha?}/{environment:alpha?}/{level:alpha?}")] HttpRequestData req,
+                                string? application,
+                                string? environment,
+                                string? level,
+                                FunctionContext executionContext)
     {
-        var logger = executionContext.GetLogger("HttpTrigger1");
-        logger.LogInformation("C# HTTP trigger function processed a request.");
+        var logger = executionContext.GetLogger("LoggerItem");
+        logger.LogInformation("POST Log item {url}.", req.Url);
+        try
+        {
+            switch (req.Method)
+            {
+                //case "GET":
+                //    break;
+                case "POST":
+                    var body = req.Body;
+                    Create(application, environment, level, body);
+                    break;
+                //case "DELETE":
+                default:
+                    throw new NotImplementedException();
+            }
 
-        var message = String.Format($"Category: {category}, ID: {id}");
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            var message = String.Format($"Category: {category}, ID: {id}");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
 
-        var content = JsonSerializer.Serialize(new { Message = message });
-        response.WriteString(content);
+            var content = JsonSerializer.Serialize(new { Message = message });
+            response.WriteString(content);
 
-        return response;
+            return response;
+        }
+        catch (ArgumentException ex)
+        {
+            return logger.HandleError(req, ex, HttpStatusCode.NotFound);
+        }
+        catch (Exception ex)
+        {
+            return logger.HandleError(req, ex, HttpStatusCode.InternalServerError);
+        }
+    }
+
+    void Create(string? application, string? environment, string? level, Stream body)
+    {
+
+        if (String.IsNullOrWhiteSpace(application)) throw new ArgumentException("Application must be set in the route.", nameof(application));
+        if (String.IsNullOrWhiteSpace(environment)) throw new ArgumentException("Environment must be set in the route.", nameof(environment));
+        if (String.IsNullOrWhiteSpace(level)) throw new ArgumentException("Level must be set in the route.", nameof(level));
+
+        return req.CreateResponse(HttpStatusCode.NotFound);
+        throw new NotImplementedException();
     }
 }
