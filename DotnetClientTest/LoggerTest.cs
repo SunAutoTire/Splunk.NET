2b20515ba96409024384982ece610615763a749c
Daniel Przybylski <daniel.przybylski@sun.auto>
2026-02-24 21:09:23 -0800

    Reorder using directives for consistency in Logger, Storage, and LoggerTest files

--- DotnetClientTest/LoggerTest.cs
+++ DotnetClientTest/LoggerTest.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System.Text;
 
 namespace SunAuto.Splunk.Client.Test;
 
